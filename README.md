533e481f1df096a330a1e3e650b6ac5fcba23d78
Nikita <sonterixo@gmail.com.com>
2018-01-05 15:56:51 +0200

    new solution

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# timer
+# Timer #
+
+> ## Files:
+#### 'timer.min.css' and 'timer.min.js' - [megatimer.ru](http://megatimer.ru/) 
+#### 'main.css' and 'main.js' - *Custom styles* 
+#### 'current_timer.js' - *Your timer* 
+#### 'index.html' - *Start file*
+***
+> ## Bugs:
+* При переходе на другую страницу в браузере - таймер сообщает об окончании работы и начинает работу заново. Это особенность(баг/фича) данного таймера.
+
+##### Удачи, в последующей работе
+##### [Nick](https://github.com/sonterix/) 
---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    setTimeout(function(){ checkTimer(); }, 500)
+}, false);
+
+function checkTimer(){
+    // track DOM changes
+    document.addEventListener("DOMSubtreeModified", function() {
+        // get minutes and secundes data
+        let minutes = document.querySelector('#timer-number-value-tmr-minutes');
+        let secundes = document.querySelector('#timer-number-value-tmr-secundes');
+    
+        // if minutes and secundes equals 0
+        if(minutes.textContent == '0' && secundes.textContent == '0'){
+            // set red color
+            let all = document.querySelectorAll('#timer-number-value-tmr-minutes, #timer-number-value-tmr-secundes, #timer-text-tmr-minutes, #timer-text-tmr-secundes');
+
+            all.forEach(function(value){
+                value.style.color = 'red';
+            });
+
+            // allerw when stop
+            alert('Timer End');
+        }
+    });
+}
